ad911432ec70f7eb936ed3fbc64e8d50c9518764
JeremiePasquis <jeremie@weboneo.fr>
2019-03-24 23:07:00 +0100

    Todolist page integration

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,19 +1,70 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="Dashboard">
+        <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>@yield('title', 'TODOLIST Organizer')</title>
 
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <!--external css-->
+        <link href="css/font-awesome.css" rel="stylesheet" />
 
-    <title>@yield('title', 'TODOLIST Organizer')</title>
+        <!-- Custom styles for this template -->
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/style-responsive.css" rel="stylesheet">
+        <link href="css/to-do.css" rel="stylesheet">
 
-</head>
-<body>
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
 
-    @yield('content')
+    <body>
+        <section id="container" >
 
-</body>
+            @yield('content')
+
+        </section>
+
+        <!-- js placed at the end of the document so the pages load faster -->
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"></script>
+        <script src="js/jquery.scrollTo.min.js"></script>
+        <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
+
+
+        <!--common script for all pages-->
+        <script src="js/common-scripts.js"></script>
+
+        <!--script for this page-->
+        <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+        <script src="js/tasks.js" type="text/javascript"></script>
+
+        <script>
+            jQuery(document).ready(function() {
+                TaskList.initTaskWidget();
+            });
+
+            $(function() {
+                $( "#sortable" ).sortable();
+                $( "#sortable" ).disableSelection();
+            });
+        </script>
+
+
+        <script>
+            //custom select box
+            $(function(){
+                $('select.styled').customSelect();
+            });
+        </script>
+    </body>
 </html>
--- resources/views/pages/todolist.blade.php
+++ resources/views/pages/todolist.blade.php
@@ -9,19 +9,64 @@
 @section('content')
 <!-- todolist.content -->
 
-    <h1>Ma TODO liste</h1>
-
+    @include('parts.top-bar')
     @include('parts.menu')
 
-    <ul>
-        @foreach ($tasks as $task)
-            <li>
-                <a href="{!! url('todolist/' . $task->id) !!}">{{ $task->task }}</a> <a href="{!! url('todolist/' . $task->id . '/edit') !!}">Éditer</a>
-            </li>
-        @endforeach
-    </ul>
+    <!-- **********************************************************************************************************************************************************
+    MAIN CONTENT
+    *********************************************************************************************************************************************************** -->
+    <!--main content start-->
+    <section id="main-content">
+        <section class="wrapper">
+            <h3><i class="fa fa-angle-right"></i> Ma TODO liste</h3>
 
-    <a href="{!! url('todolist/create') !!}">Nouvelle tâche</a>
+            <!-- COMPLEX TO DO LIST -->
+            <div class="row mt">
+                <div class="col-md-12">
+                    <section class="task-panel tasks-widget">
+                        <div class="panel-heading">
+                            <div class="pull-left"><h5><i class="fa fa-tasks"></i> Todo List - Complex Style</h5></div>
+                            <br>
+                        </div>
+                        <div class="panel-body">
+                            <div class="task-content">
+
+                                <ul class="task-list">
+
+                                    @foreach ($tasks as $task)
+                                        <li>
+                                            <div class="task-checkbox">
+                                                <input type="checkbox" class="list-child" value=""  />
+                                            </div>
+                                            <div class="task-title">
+                                                <span class="task-title-sp">{{ $task->task }}</span>
+                                                @foreach ($task->ponderators as $ponderator)
+                                                    <span class="badge bg-theme">{{ $ponderator->name }}</span>
+                                                @endforeach
+                                                <div class="pull-right hidden-phone">
+                                                    <button class="btn btn-success btn-xs"><i class=" fa fa-check"></i></button>
+                                                    <a class="btn btn-primary btn-xs" href="{!! url('todolist/' . $task->id . '/edit') !!}"><i class="fa fa-pencil"></i></a>
+                                                    <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    @endforeach
+
+                                </ul>
+                            </div>
+
+                            <div class=" add-task-row">
+                                <a class="btn btn-success btn-sm pull-left" href="{!! url('todolist/create') !!}">Nouvelle tâche</a>
+                            </div>
+                        </div>
+                    </section>
+                </div><!-- /col-md-12-->
+            </div><!-- /row -->
+
+        </section><!-- /wrapper -->
+    </section><!-- /MAIN CONTENT -->
+
+    <!--main content end-->
 
 <!-- /todolist.content -->
 @endsection
